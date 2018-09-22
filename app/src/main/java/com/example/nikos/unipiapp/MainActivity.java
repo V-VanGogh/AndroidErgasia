ca56eb8e47f81f074504a93ff7652f897265ab81
Nikos Math <nmathioudis@gmail.com>
2018-09-22 12:22:41 +0300

    For Loop getArticles().get(i).getTitle().toString()

--- app/src/main/java/com/example/nikos/unipiapp/MainActivity.java
+++ app/src/main/java/com/example/nikos/unipiapp/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -16,10 +19,11 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends DropDownMenu{
+public class MainActivity extends DropDownMenu {
 
     ListView listView;
     FirebaseAuth mAuth;
+    ArrayList<String> newsArticles = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +31,13 @@ public class MainActivity extends DropDownMenu{
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
 
+
         listView = findViewById(R.id.listNews);
 
         getNews();
     }
 
-    private  void getNews() {
+    private void getNews() {
         // Retrofit object
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(NewsInterface.BASE_URL)
@@ -49,21 +54,24 @@ public class MainActivity extends DropDownMenu{
                     NewsDataModel news = response.body();
                     Log.i("Status:", news.getStatus());
                     Log.i("TotalResult:", news.getTotalResults().toString());
-                    for (int i=0; i<news.getArticles().size();i++) {
+                    for (int i = 0; i < news.getArticles().size(); i++) {
                         Log.i("Articles:", news.getArticles().get(i).getTitle().toString());
+                        newsArticles.add(news.getArticles().get(i).getTitle().toString());
+
                     }
                 } else {
                     Log.e("Failed:", "das" + response.code());
                 }
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(listView.getContext(), android.R.layout.simple_list_item_1, newsArticles);
+                listView.setAdapter(adapter);
             }
 
             @Override
             public void onFailure(Call<NewsDataModel> call, Throwable t) {
-                Log.e("Failed error", "dasdasdsada"+t.getMessage());
+                Log.e("Failed error", "dasdasdsada" + t.getMessage());
             }
         });
     }
-
 
 
     @Override
